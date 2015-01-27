7be5265700b450c194dcfa9980b8f5ad8a36d5e1
Christian Volmering <christian@volmering.name>
2015-01-27 12:52:50 +0100

    Beta release of the Extension

--- scripts/cisco_dialer_event.js
+++ scripts/cisco_dialer_event.js
@@ -1,6 +1,6 @@
 /*
  *   Cisco Dialer - Chrome Extension
- *   Copyright (C) 2014 Christian Volmering <christian@volmering.name>
+ *   Copyright (C) 2015 Christian Volmering <christian@volmering.name>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
